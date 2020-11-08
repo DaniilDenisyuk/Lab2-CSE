5a7664a5585277ed75423c891f5a81e63888418a
Daniil Denisyuk <ordekonec@gmail.com>
2020-11-08 22:38:12 +0200

    Updated to proper requirements

--- Lab2/UnitTest1.cs
+++ Lab2/UnitTest1.cs
@@ -1,29 +1,82 @@
 using System;
-using Xunit;
 using IIG.BinaryFlag;
+using Xunit;
 
 namespace Lab2
 {
-    public class UnitTest1
+    public class BinaryFlagTest
     {
-        private MultipleBinaryFlag _multipleBinaryFlag;
-        public UnitTest1()
+        [Theory]
+        [InlineData(18446744073709551615)]
+        public void MBF_Constructor_ULongLengthTest_Exception(ulong l)
         {
-            _multipleBinaryFlag = new MultipleBinaryFlag(10);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(l));
         }
-        [Fact]
-        public void Test1()
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void MBF_Constructor_LengthTest_Exception(ulong l)
         {
-            Assert.True(_multipleBinaryFlag.GetFlag());
-            _multipleBinaryFlag.ResetFlag(4);
-            Assert.False(_multipleBinaryFlag.GetFlag());
-            _multipleBinaryFlag.SetFlag(4);
-            Assert.True(_multipleBinaryFlag.GetFlag());
-            _multipleBinaryFlag.Dispose();
-            //OBJECT IS NOT DISPOSED!!!
-            Assert.Null(_multipleBinaryFlag);
-            _multipleBinaryFlag = new MultipleBinaryFlag(5, false);
-            Assert.False(_multipleBinaryFlag.GetFlag());
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MultipleBinaryFlag(l));
+        }
+        
+        [Theory]
+        [InlineData(100, true)]
+        [InlineData(400, true)]
+        [InlineData(213, true)]
+        public void MBF_Constructor_Length_ResTrue(ulong l, bool v)
+        {
+            var mbf = new MultipleBinaryFlag(l, v);
+            Assert.Equal(v, mbf.GetFlag());
+        }
+        
+        [Theory]
+        [InlineData(100, false)]
+        [InlineData(400, false)]
+        [InlineData(213, false)]
+        public void MBF_Constructor_Length_ResFalse(ulong l, bool v)
+        {
+            var mbf = new MultipleBinaryFlag(l, v);
+            Assert.Equal(v, mbf.GetFlag());
+        }
+        
+        [Theory]
+        [InlineData(400, false, false)]
+        [InlineData(400, true, true)]
+        [InlineData(2, false, true)]
+        public void MBF_GetFlag_SetFlag(ulong l, bool v, bool expected)
+        {
+            var mbf = new MultipleBinaryFlag(l, v);
+            Assert.Equal(v, mbf.GetFlag());
+            mbf.SetFlag(0);
+            mbf.SetFlag(1);
+            Assert.Equal(expected, mbf.GetFlag());
+        }
+        
+        [Theory]
+        [InlineData(400, false, false)]
+        [InlineData(400, true, false)]
+        [InlineData(2, true, false)]
+        public void MBF_GetFlag_ResetFlag(ulong l, bool v, bool expected)
+        {
+            var mbf = new MultipleBinaryFlag(l, v);
+            Assert.Equal(v, mbf.GetFlag());
+            mbf.ResetFlag(1);
+            Assert.Equal(expected, mbf.GetFlag());
+        }
+
+        [Theory]
+        [InlineData(400, false, false)] 
+        [InlineData(400, true, true)] 
+        [InlineData(2, true, true)]
+        public void MBF_GetFlag_SetFlag_ResetFlag(ulong l, bool v, bool expected)
+        {
+            var mbf = new MultipleBinaryFlag(l, v);
+            Assert.Equal(v, mbf.GetFlag());
+            mbf.ResetFlag(1);
+            mbf.SetFlag(1);
+            Assert.Equal(expected, mbf.GetFlag());
         }
     }
 }
